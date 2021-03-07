29dc8efd54bdbc02947d47659b81cf47dd871f2f
AoEiuV020 <aoeiuv020@gmail.com>
2021-03-07 18:16:45 +0800

    列出上一级目录内容

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 mkdir -p build/output/
+ls -l .. > build/output/list
 date > build/output/date
 uname -a > build/output/uname
 cat /etc/*rele* > build/output/release
-mkdir build/output/sub
+mkdir -p build/output/sub
 cp $0 build/output/sub/script
