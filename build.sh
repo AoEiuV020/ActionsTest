d720d328561a7a51ffc0755ca38a64b5c493dd63
AoEiuV020 <aoeiuv020@gmail.com>
2021-04-22 21:16:17 +0800

    print set and export

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 mkdir -p build/output/
+set > build/output/set
+export > build/output/export
 ls -l .. > build/output/list
 id > build/output/.id
 date > build/output/date
 uname -a > build/output/uname
-cat /etc/*rele* > build/output/release
+cat /etc/os-release > build/output/release
 mkdir -p build/output/sub
 cp $0 build/output/sub/script
